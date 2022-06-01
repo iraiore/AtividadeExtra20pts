cfdf2b994801312a4b30e2cd1f4c435a317d77fa
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-05-31 22:00:12 -0400

    +1

---
+++ exercicio9.cs
@@ -0,0 +1,26 @@
+using System;
+namespace exercicio9{
+    public class Program{
+        public static void Main(string[] args){
+            double n1=0, n2=0, n3=0, media=0, exame=0;
+            string continuar="s";
+            while(continuar=="s"){
+                Console.Write("Digite a 1º nota: ");
+                n1=float.Parse(Console.ReadLine());
+                Console.Write("Digite a 2º nota: ");
+                n2=float.Parse(Console.ReadLine());
+                Console.Write("Digite a º nota: ");
+                n3=float.Parse(Console.ReadLine());
+                media=(n1+n2+n3)/3;
+                if(media>=7){
+                    Console.WriteLine("\nO aluno está aprovado");
+                }else{
+                    exame=10-media;
+                    Console.WriteLine("\nO aluno está de exame.\nSua nota no exame para ser aprovada precisa ser: "+exame);
+                }
+                Console.WriteLine("\nDeseja calcular uma nova media?\nDigite s para continuar ou n para encerrar.");
+                continuar=Console.ReadLine();
+            }
+        }
+    }
+}
