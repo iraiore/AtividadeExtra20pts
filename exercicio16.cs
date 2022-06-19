6a3d8fb830f4b481e9769c134677fa323fcd3c81
Iraiore <90807410+iraiore@users.noreply.github.com>
2022-06-19 17:54:05 -0400

    mais 1

--- exercicio16.cs
+++ exercicio16.cs
@@ -2,21 +2,31 @@ using System;
 namespace exercicio16{
     public class Program{
         public static void Main(string[] args){
-            int numero_total=0;
-            double mediana=0;
+            int numero_elementos=0,metade_numero_elementos=0;
+            double mediana=0, mediana1=0, mediana2=0;
             Console.WriteLine("Entre com o numero inicial da série: ");
             int inicial = int.Parse(Console.ReadLine());
             Console.WriteLine("Entre com o numero final da serie: ");
             int final = int.Parse(Console.ReadLine());
+            Console.Write("A sequência é: ");
             for(int i=inicial; i<=final; i++){
-                numero_total=numero_total+i;
+                numero_elementos=i;
+                Console.Write(numero_elementos+" ");
             }
-            if (numero_total%2!=0){
-                numero_total=(numero_total-1)/2;
-                for(int i=1; i<=numero_total;i++){
-                    mediana = mediana+(double)i;
+            if (numero_elementos%2!=0){
+                metade_numero_elementos=(numero_elementos+1)/2;
+                for(int i=inicial; i<=metade_numero_elementos;i++){
+                    mediana = i;
                 }
-                Console.WriteLine("A mediana da sequencia é: "+mediana);
+                Console.WriteLine("\nA mediana da sequencia é: "+mediana);
+            }else{
+                metade_numero_elementos=numero_elementos/2;
+                for(int i=inicial; i<=metade_numero_elementos+1;i++){
+                    mediana1=i-1;
+                    mediana2=i;
+                }
+                mediana=(mediana1+mediana2)/2;
+            Console.WriteLine("\nA mediana da sequencia é: "+mediana);
             }
         }
     }
